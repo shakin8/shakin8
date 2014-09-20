10928261efdef4799a5549efa93d5f8f31744ab6
shakin8 <beattie.rory@gmail.com>
2014-09-20 13:16:56 -0400

    environment assett precompile

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@
         I18n.enforce_available_locales = true
 
         config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-        config.assets.initialize_on_precompile = false
+   
       end
     end
